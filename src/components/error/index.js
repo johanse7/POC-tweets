72afa1f50af6dce3e163e6f8f4f382b7a7242964
Sebastian Rodriguez Sanchez <xcbsrodr@intcomex.com>
2020-10-30 18:22:58 -0500

    add build command

--- src/components/error/index.js
+++ src/components/error/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-// import errorImage from '../../assets/errorPage.png';
+import errorImage from '../../assets/errorPage.png';
 import './error.scss';
 
 const Error = () => (
   <div className='error-detail'>
-    {/* <img src={errorImage} alt='error' /> */}
+    <img src={errorImage} alt='error' />
     <p>Error server</p>
   </div>
 );
