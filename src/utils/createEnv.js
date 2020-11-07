cdb9026e61f4c514ef17db4a61b5ce76142825dc
Sebastian Rodriguez Sanchez <xcbsrodr@intcomex.com>
2020-11-07 16:53:44 -0500

    add url proxy tweets

--- src/utils/createEnv.js
+++ src/utils/createEnv.js
@@ -4,3 +4,4 @@ fs.writeFileSync('./.env', `URL_API_TWITTER=${process.env.URL_API_TWITTER}\n`);
 fs.writeFileSync('./.env', `TOKEN=${process.env.TOKEN}\n`);
 fs.writeFileSync('./.env', `PER_PAGE=${process.env.PER_PAGE}\n`);
 fs.writeFileSync('./.env', `URL_GET_TWEET=${process.env.URL_GET_TWEET}\n`);
+fs.writeFileSync('./.env', `URL_PROXY_API=${process.env.URL_PROXY_API}\n`);
